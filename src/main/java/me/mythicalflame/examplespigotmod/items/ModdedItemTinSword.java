d26d2ed45375c7ff71e1f9337b512cdef5e50159
MythicalFlame <82922845+MythicalFlame@users.noreply.github.com>
2022-12-30 14:54:05 -0600

    Initial commit

---
+++ src/main/java/me/mythicalflame/examplespigotmod/items/ModdedItemTinSword.java
@@ -0,0 +1,13 @@
+package me.mythicalflame.examplespigotmod.items;
+
+import me.mythicalflame.spigotmodding.items.ModdedItem;
+import org.bukkit.Material;
+
+public class ModdedItemTinSword extends ModdedItem
+{
+    public ModdedItemTinSword()
+    {
+        super("example", "tin_sword", Material.IRON_SWORD, "Tin Sword");
+    }
+    //TODO: extra damage/sword functionality
+}
---
+++ src/main/java/me/mythicalflame/examplespigotmod/items/ModdedItemWalkingStick.java
@@ -0,0 +1,12 @@
+package me.mythicalflame.examplespigotmod.items;
+
+import me.mythicalflame.spigotmodding.items.ModdedItem;
+import org.bukkit.Material;
+
+public class ModdedItemWalkingStick extends ModdedItem
+{
+    public ModdedItemWalkingStick()
+    {
+        super("example", "walking_stick", Material.STICK, "Walking Cane");
+    }
+}
